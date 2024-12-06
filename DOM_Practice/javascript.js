a2078ede7533f76ccd45ac45d925d57341481dca
jack <jackfine01@gmail.com>
2024-12-06 15:12:14 -0500

    adding intital js.

--- DOM_Practice/javascript.js
+++ DOM_Practice/javascript.js
@@ -5,3 +5,17 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
+
+const para = document.createElement("p");
+para.classList.add("paragraph");
+p.style.color = "red";
+para.textContent("Hey, I'm red!");
+
+const threehead = document.createElement("h3");
+threehead.classList.add("threehead");
+h3.style.color = "blue";
+threehead.textContent("I'm a blue h3!");
+
+const blackdiv = document.createElement("div");
+blackdiv.classList.add("blackdiv");
+div.style.color = "black";
